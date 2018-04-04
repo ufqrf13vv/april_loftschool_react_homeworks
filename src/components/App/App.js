0afb431869a468f989fda4ddb212abb82d70f07b
Mikhail Ragulin <anarchy16@inbox.ru>
2018-04-04 16:28:02 +0600

    homework 1 complete

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <p className="descriptoin">Миру-мир, студентам - beer.</p>
+            </div>
+        );
+    }
+}
+
+export default App;
